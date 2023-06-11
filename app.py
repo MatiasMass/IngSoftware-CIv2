2b6ba3bd22a5ea12783a2aa32ac454464b7304ef
MatiasMass <matiasmass14@gmail.com>
2023-06-10 21:25:03 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+message = "Flask is working"
+# Define the root route
+@app.route('/')
+def index():
+    return render_template('index.html', message = message)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ test.py
@@ -0,0 +1,16 @@
+from app import app
+
+def test_data():
+    response = app.test_client().get('/')
+    expected = b"""
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Ingenieria Software CI</title>
+  </head>
+  <body>
+    <h1>Flask is working</h1>
+  </body>
+</html>"""
+    assert response.data.strip() == expected.strip()
+
